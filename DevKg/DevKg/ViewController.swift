7112fc35ac005248c3ebe6cd6f6e8a99d2198378
sher304 <esherovshermat@gmail.com>
2022-09-03 19:14:19 +0600

    :tada: Begin The Project

---
+++ DevKg/DevKg/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DevKg
+//
+//  Created by Шермат Эшеров on 3/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
